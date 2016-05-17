356a41deab8a7d3a13bbfb110fe1ff24c995de69
Johannes Donath <johannesd@torchmind.com>
2016-05-17 16:48:25 +0200

    Code style.

--- common/src/main/java/com/torchmind/stockpile/data/v1/ServerInformation.java
+++ common/src/main/java/com/torchmind/stockpile/data/v1/ServerInformation.java
@@ -101,7 +101,7 @@ public class ServerInformation {
         @Override
         public boolean equals(Object o) {
                 if (this == o) return true;
-                if (o == null || getClass() != o.getClass()) return false;
+                if (o == null || this.getClass() != o.getClass()) return false;
                 ServerInformation that = (ServerInformation) o;
 
                 return Objects.equals(this.name, that.name) &&
